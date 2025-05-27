d1a6824199232d8365e569f64971355672818705
DennisTurco <dennisturco@gmail.com>
2025-05-27 15:09:33 +0200

    custom font for the menu

--- Menu.py
+++ Menu.py
@@ -9,17 +9,38 @@ class Menu:
         self.mode = None
         self.elo = 1200
 
+        custom_theme = themes.THEME_SOLARIZED.copy()
+        custom_theme.title_font = pygame_menu.font.FONT_MUNRO
+        custom_theme.widget_font = pygame_menu.font.FONT_MUNRO
+        custom_theme.title_font_size = 60
+        custom_theme.widget_font_size = 35
+
         programIcon = pygame.image.load('images/wK.png')
         pygame.display.set_icon(programIcon)
 
-        self.main_menu = pygame_menu.Menu(f'Welcome to {GameManager.APP_NAME}', GameManager.WINDOW_WIDTH, GameManager.HEIGHT, theme=themes.THEME_SOLARIZED)
-        self.mode_menu = pygame_menu.Menu('Modes', GameManager.WINDOW_WIDTH, GameManager.HEIGHT, theme=themes.THEME_SOLARIZED)
+        self.main_menu = pygame_menu.Menu(
+            title=f'Welcome to {GameManager.APP_NAME}',
+            width=GameManager.WINDOW_WIDTH,
+            height=GameManager.HEIGHT,
+            theme=custom_theme
+        )
+        self.mode_menu = pygame_menu.Menu(
+            title='Modes',
+            width=GameManager.WINDOW_WIDTH,
+            height=GameManager.HEIGHT,
+            theme=custom_theme
+        )
 
         self.mode_menu.add.button('Player vs Player', self.select_pvp)
         self.mode_menu.add.button('Player vs AI', self.elo_menu)
         self.mode_menu.add.button('Back', events.BACK)
 
-        self.elo_select_menu = pygame_menu.Menu('Select AI Difficulty (ELO)', GameManager.WINDOW_WIDTH, GameManager.HEIGHT, theme=themes.THEME_SOLARIZED)
+        self.elo_select_menu = pygame_menu.Menu(
+            title='Select AI Difficulty (ELO)',
+            width=GameManager.WINDOW_WIDTH,
+            height=GameManager.HEIGHT,
+            theme=custom_theme
+        )
         self.elo_select_menu.add.selector('ELO level:', [
             ('400', 400), ('600', 600), ('800', 800), ('1000', 1000), ('1200', 1200), ('1500', 1500),
             ('1800', 1800), ('2000', 2000), ('2200', 2200), ('2500', 2500),
@@ -33,7 +54,7 @@ class Menu:
 
         self.surface = None
 
-    def set_elo(self, value: int) -> None:
+    def set_elo(self, selected, value: int) -> None:
         self.elo = value
 
     def elo_menu(self) -> None:
